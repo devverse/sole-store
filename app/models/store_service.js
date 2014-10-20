5ca44085d586d7e818e3b274e62c077e46767f4c
Robbins Cleozier <rcleozier@gmail.com>
2014-10-20 18:31:03 -0400

    Added more assets

---
+++ app/models/store_service.js
@@ -0,0 +1,66 @@
+soleSalesApp.factory('store_service', ['$rootScope', '$q', '$http',
+  function($rootScope, $q, $http) {
+
+    var api = serviceURL;
+
+    self.makePost = function(endpoint, post) {
+
+      post = (!post) ? {} : post;
+      if (!endpoint) {
+        window.alert("Could not connect to database");
+        return;
+      }
+
+      var deferred = $q.defer();
+      $http.post(api + endpoint, post).success(function(data) {
+
+
+        if (data) {
+          if (data == 'false') {
+            data = [];
+          }
+          deferred.resolve(data);
+        } else {
+          deferred.reject("Data was rejected: " + post);
+        }
+      });
+      return deferred.promise;
+
+    };
+
+    self.getMenu = function() {
+      return self.makePost('/store/getMenu');
+    };
+
+    self.search = function(search) {
+      var post = "search=" + search;
+      return self.makePost('/store/search', post);
+    };
+
+    self.paginate = function(post) {
+      return self.makePost('/store/paginate', post);
+    };
+
+    self.getDefaultItems = function() {
+      return self.makePost('/store/getDefaultItems', '');
+    };
+
+
+    return {
+      getMenu: function() {
+        return self.getMenu();
+      },
+
+      search: function(search) {
+        return self.search(search);
+      },
+
+      getDefaultItems: function() {
+        return self.getDefaultItems();
+      },
+      paginate: function(post) {
+        return self.paginate(post);
+      }
+    };
+  }
+]);
---
+++ bower_components/h5validate/README.md
@@ -0,0 +1,39 @@
+h5validate
+==========
+
+Shim repository for the [h5validate](https://github.com/dilvie/h5Validate) jQuery plugin.
+
+Original demo page can be found [here](http://ericleads.com/h5validate/).
+
+_Note: function `h5Validate` as been lowercased (i.e., `h5validate`)._
+
+Installation
+------------
+
+### Using [composer](https://github.com/composer/composer)
+    
+To install with [composer](http://packagist.org/packages/components/h5validate) add `components/h5validate` to your `composer.json`. E.g.,
+
+```
+{
+  "require": {
+    "components/h5validate": "*"
+  }
+}
+```
+
+### Using [component](http://component.io)
+    
+To install as a [component](https://github.com/component/component) just run `component install components/h5validate`.
+
+Or add `components/h5validate` to the dependencies element into your `component.json`.
+
+### Using [bower](http://bower.io)
+
+To install with [bower]() just run `bower install h5validate`.
+
+Or add `h5validate` to the dependencies element into you `bower.json`.
+
+---
+
+[![Analytics](https://ga-beacon.appspot.com/UA-49657176-1/h5validate)](https://github.com/igrigorik/ga-beacon)
